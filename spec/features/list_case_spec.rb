9c1cc3ce2ce76049a0d2804f95a8623e53d955be
jrsahuquillo <jrsahuquillo@gmail.com>
2019-08-16 03:25:21 +0200

    Displays message when no drawers present ✅

--- spec/features/list_case_spec.rb
+++ spec/features/list_case_spec.rb
@@ -7,7 +7,7 @@ RSpec.feature "Listing Drawers" do
       @drawer2 = Drawer.create(title: "The second drawer", description: "Description of second drawer")
   end
 
-  scenario "a user creates a new drawer" do
+  scenario "a user lists all drawers" do
     visit "/"
 
     expect(page).to have_content(@drawer1.title)
@@ -16,6 +16,23 @@ RSpec.feature "Listing Drawers" do
     expect(page).to have_content(@drawer2.description)
     expect(page).to have_link(@drawer1.title)
     expect(page).to have_link(@drawer1.title)
+  end
+
+  scenario "a user has no drawers" do
+    Drawer.delete_all
+
+    visit "/"
+
+    expect(page).not_to have_content(@drawer1.title)
+    expect(page).not_to have_content(@drawer1.description)
+    expect(page).not_to have_content(@drawer2.title)
+    expect(page).not_to have_content(@drawer2.description)
+    expect(page).not_to have_link(@drawer1.title)
+    expect(page).not_to have_link(@drawer1.title)
+
+    within("h1#no-drawers") do
+      expect(page).to have_content("No drawers created")
+    end
   end
 
 end
