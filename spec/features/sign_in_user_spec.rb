a013af43332cad7aa26699f8dec409e39a63572d
jrsahuquillo <jrsahuquillo@gmail.com>
2019-08-18 11:35:34 +0200

    Implements user sign in ✅

---
+++ spec/features/sign_in_user_spec.rb
@@ -0,0 +1,24 @@
+require "rails_helper"
+
+RSpec.feature "Signin users" do
+
+  before do
+    @user = User.create!(email: "example_user@example.com", password: "password")
+  end
+
+  scenario "with valid credentials" do
+    visit "/"
+    click_link "Sign in"
+    fill_in "Email", with: @user.email
+    fill_in "Password", with: @user.password
+    click_button "Log in"
+
+    expect(page).to have_content("Signed in successfully.")
+    expect(page).to have_content("Signed in as #{@user.email}")
+    expect(page).not_to have_link("Sign in")
+    expect(page).not_to have_link("Sign up")
+    # expect(page).to have_link("Log out")
+
+  end
+
+end
--- spec/features/sign_up_user_spec.rb
+++ spec/features/sign_up_user_spec.rb
@@ -4,7 +4,7 @@ RSpec.feature "Signup users" do
   scenario "with valid credentials" do
     visit "/"
     click_link "Sign up"
-    fill_in "Email", with: "user@example.com"
+    fill_in "Email", with: "user-example@example.com"
     fill_in "Password", with: "password"
     fill_in "Password confirmation", with: "password"
     click_button "Sign up"
