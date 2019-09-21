3f0cf1d1e81eaf1ade9df960c4eec0a1e46237a0
jrsahuquillo <jrsahuquillo@gmail.com>
2019-09-21 09:38:15 +0200

    Edit codetool WIP

---
+++ spec/features/edit_codetool_spec.rb
@@ -0,0 +1,29 @@
+require "rails_helper"
+
+RSpec.feature "Editing Codetools" do
+
+  before do
+      user = User.create(email: "example_user@example.com", password: "password")
+      login_as(user)
+      @drawer = Drawer.create(title: "The first drawer", description: "Description of first drawer", user: user)
+      @codetool = Codetool.create(title: "The first codetool", content: "Content of first codetool", drawer: @drawer, user: user)
+  end
+
+  scenario "A user updates a codetool" do
+    visit "/"
+    click_link @drawer.title
+    find('.show-codetool').click
+    click_link "Edit Codetool"
+
+    fill_in "Title", with: "Updated Title"
+    fill_in "Content", with: "Updated Content of Codetool"
+    click_button "Update Codetool"
+
+    expect(page).to have_content("Codetool has been updated")
+    expect(page).to have_content("Updated Title")
+    expect(page).to have_content("Updated Content of Codetool")
+    expect(page.current_path).to eq(drawer_path(@drawer))
+  end
+
+
+end
