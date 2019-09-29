3597380b2ca8bcf27e926b8527482e2b5b8f8109
jrsahuquillo <jrsahuquillo@gmail.com>
2019-09-29 09:31:16 +0200

    Adds username attribute to user model 👤

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-
   rescue_from ActiveRecord::RecordNotFound, with: :resource_not_found
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
+    devise_parameter_sanitizer.permit(:sign_in, keys: [:username])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:username])
+  end
 end
--- spec/features/sign_in_user_spec.rb
+++ spec/features/sign_in_user_spec.rb
@@ -3,12 +3,13 @@ require "rails_helper"
 RSpec.feature "Signin users" do
 
   before do
-    @user = User.create!(email: "example_user@example.com", password: "password")
+    @user = User.create!(username: "example_user", email: "example_user@example.com", password: "password")
   end
 
   scenario "with valid credentials" do
     visit "/"
     click_link "Sign in"
+    fill_in "Username", with: @user.username
     fill_in "Email", with: @user.email
     fill_in "Password", with: @user.password
     click_button "Log in"
