c3deb24f9e60fe178f399f156ad341e6a83d9b0b
jrsahuquillo <jrsahuquillo@gmail.com>
2019-09-29 08:47:21 +0200

    Displays Codetool after editing it 🔧

--- app/javascript/packs/custom/codetools.js
+++ app/javascript/packs/custom/codetools.js
@@ -1,8 +1,17 @@
 $(document).ready(function(){
+  collapseShowAfterEdit();
 
   $('.show-codetool').click(function () {
     var codetoolId = this.attributes['aria-controls'].value
     $('.octicon.octicon-chevron-down.' + codetoolId).toggleClass('rotated')
   });
-  
+
+  function collapseShowAfterEdit(){
+    var params_referrer = document.referrer.split('/').slice(-2)
+    if(params_referrer[1] == 'edit'){
+      var codetoolItem = '#codetool' + params_referrer[0]
+      $(codetoolItem).addClass('show')
+    }
+  }
+
 });
