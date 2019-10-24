00f2df59db563c6cf80a446bcd3d637fcc71d50e
jrsahuquillo <jrsahuquillo@gmail.com>
2019-10-24 16:27:19 +0200

    Enhaces performance 🚀

--- spec/features/search_codetool_spec.rb
+++ spec/features/search_codetool_spec.rb
@@ -4,16 +4,15 @@ RSpec.feature "Searching Codetools" do
   before do
     user = User.create!(email: "example_user@example.com", username: "example_user", password: "password")
     login_as(@user)
-    drawer = Drawer.create(title: "The first drawer", description: "Description of first drawer", user: @user)
-    @codetool1 = Codetool.create(title: "The first codetool", content: "Content of first codetool", drawer: drawer, user: user)
-    @codetool2 = Codetool.create(title: "The second codetool", content: "Content of second codetool", drawer: drawer, user: user)
+    @drawer = Drawer.create(title: "The first drawer", description: "Description of first drawer", user: @user)
+    @codetool1 = Codetool.create(title: "The first codetool", content: "Content of first codetool", drawer: @drawer, user: user)
+    @codetool2 = Codetool.create(title: "The second codetool", content: "Content of second codetool", drawer: @drawer, user: user)
   end
 
   scenario "a user searches a specific codetool title" do
     visit '/search'
     fill_in "Search", with: "first"
     find(:css, '.search').click
-    # click_button "Search"
 
     expect(page).to have_content(@codetool1.title)
     expect(page).not_to have_content(@codetool2.title)
@@ -24,7 +23,6 @@ RSpec.feature "Searching Codetools" do
     visit '/search'
     fill_in "Search", with: "First coDEtool"
     find(:css, '.search').click
-    # click_button "Search"
 
     expect(page).to have_content(@codetool1.title)
     expect(page).not_to have_content(@codetool2.title)
