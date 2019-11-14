40d91f1de280f795abf4f422601b6d9ae2ce14d3
jrsahuquillo <jrsahuquillo@gmail.com>
2019-11-14 17:59:19 +0100

    Submits form when presses cmd+enter keys ⌨️

--- app/javascript/packs/custom/codetools.js
+++ app/javascript/packs/custom/codetools.js
@@ -15,4 +15,13 @@ $(document).ready(function(){
     }
   }
 
+
+  $(document).keydown(function(e) {
+    if(!(e.keyCode == 13 && e.metaKey)) return;
+    var target = e.target;
+    if(target.form) {
+      target.form.submit();
+    }
+  });
+
 });
