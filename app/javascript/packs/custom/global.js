faf682030afabb12d58007d04f31a90345037a90
jrsahuquillo <jrsahuquillo@gmail.com>
2019-12-12 19:13:50 +0100

    Creates key shortcut to focus on searching form input 🎯

--- app/javascript/packs/custom/global.js
+++ app/javascript/packs/custom/global.js
@@ -1,11 +1,17 @@
 $(document).ready(function(){
   // CMD + Intro save shortcut
   $(document).keydown(function(e) {
-    if(!(e.keyCode == 13 && e.metaKey)) return;
+    if(!(e.keyCode == 13 && (e.ctrlKey || e.metaKey))) return;
     var target = e.target;
     if(target.form) {
       target.form.submit();
     }
+  });
+
+  // CRTL + F Search shortcut
+  $(document).keydown(function(e) {
+    if(!(e.keyCode == 70 && e.ctrlKey)) return;
+    $('#search').val("").focus();
   });
 
 });
