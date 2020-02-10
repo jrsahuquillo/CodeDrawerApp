89d0c2cce2373bbe7357ec29f73b10f51e7423f3
jrsahuquillo <jrsahuquillo@gmail.com>
2020-02-10 08:38:33 +0100

    Adds collaborators to drawer information 👥

--- app/helpers/drawers_helper.rb
+++ app/helpers/drawers_helper.rb
@@ -10,4 +10,20 @@ module DrawersHelper
   def sorted_drawers
     current_user.drawers.sort_by(&:position)
   end
+
+  def drawer_title(drawer)
+    "#{drawer.title.truncate(50)}&nbsp;&nbsp;#{octicon 'organization' if drawer.friends.present?}".html_safe
+  end
+
+  def drawer_collaborators
+    if @drawer.friends.present?
+      drawer_friends = ""
+      @drawer.friends.each do |friend|
+        friend = friend.username
+        drawer_friends << "#{content_tag :span, friend, class: 'badge badge-secondary'}&nbsp;"
+      end
+      "<span>Collaborators: #{drawer_friends}</span>".html_safe
+    end
+  end
+
 end
