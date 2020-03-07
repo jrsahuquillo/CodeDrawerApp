b54737b0e71e9598c19bf65569dd2208c31732c0
jrsahuquillo <jrsahuquillo@gmail.com>
2020-03-07 12:50:54 +0100

    Crreates collaboration notifications and refactors notifications code

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -2,8 +2,7 @@ class NotificationsController < ApplicationController
   before_action :authenticate_user!
   before_action :set_notifications
 
-  def index
-  end
+  def index; end
 
   def mark_as_read
     @notifications.update_all(read_at: Time.zone.now)
@@ -13,7 +12,6 @@ class NotificationsController < ApplicationController
   private
 
   def set_notifications
-    # @notifications = Notification.where(recipient: current_user).unread
     @notifications = current_user.notifications.unread
   end
 
--- app/javascript/packs/custom/notifications.js
+++ app/javascript/packs/custom/notifications.js
@@ -1,24 +1,35 @@
 $(document).on('turbolinks:load',function(){
-
   //GET notifications
   $.get('/notifications.json', function(data, status) {
-    if(data.length > 0) {
+    displayNotifications(data);
+  });
+
+  function displayNotifications(data) {
+    if (data.length > 0) {
       var total_notifications = data.length
       $('.notifications-badge').text(total_notifications);
-      for(var i = 0; i < total_notifications; i++) {
-        if (data[i].notifiable["type"] == "Friendship") {
-          $('#notifications .dropdown-menu').append(
-            "<div class='dropdown-item' id='notification_" + data[i].id + "'>" + data[i].actor + " " + data[i].action + " you</div>"
-          );
-        } else if (data[i].notifiable["type"] == "Drawer") {
-          $('#notifications .dropdown-menu').append(
-            "<div class='dropdown-item' id='notification_" + data[i].id + "'>" + data[i].actor + " " + data[i].action + " of a drawer</div>"
-          );
-        }
 
-      }
+      for(var i = 0; i < total_notifications; i++) {
+        filterNotifications(data[i]);
+      };
+    };
+  };
+
+  function filterNotifications(notification) {
+    if (notification.notifiable["type"] == "Friendship") {
+      $('#notifications .dropdown-menu').append(
+        "<div class='dropdown-item' id='notification_" + notification.id + "'>" + notification.actor + " " + notification.action + " you</div>"
+      );
+    } else if (notification.notifiable["type"] == "Drawer") {
+      $('#notifications .dropdown-menu').append(
+      "<div class='dropdown-item' id='notification_" + notification.id + "'>" + notification.actor + " " + notification.action + " of a drawer</div>"
+      );
+    } else if (notification.notifiable["type"] == "User") {
+      $('#notifications .dropdown-menu').append(
+        "<div class='dropdown-item'id='notification_" + notification.id + "'> Welcome to notifications! </div>"
+      );
     }
-  });
+  };
 
   //POST read notifications
   $('#notifications').on('click', function() {
