e700084d82b11cb9fb121ae72a564c21dc9b3f4d
jrsahuquillo <jrsahuquillo@gmail.com>
2020-04-05 10:39:22 +0200

    Includes displaying codetool detail tests

--- spec/features/codetools/show_codetool_spec.rb
+++ spec/features/codetools/show_codetool_spec.rb
@@ -13,9 +13,30 @@ RSpec.feature "Showing a Codetool" do
     click_link @drawer.title
 
     expect(page).to have_content(@codetool.title)
-
-    expect(page).to have_content(@codetool.title)
     expect(page).to have_content(@codetool.content)
     expect(current_path).to eq(drawer_codetools_path(@drawer))
   end
+
+  scenario "a user shows detail of a codetool that belongs a drawer" do
+    visit "/"
+    click_link @drawer.title
+    find(:css, '.detail-codetool-icon').click
+
+    expect(page).to have_content(@codetool.title)
+    expect(page).to have_content(@codetool.content)
+    expect(current_path).to eq(drawer_codetool_path(@drawer, @codetool))
+  end
+
+  # scenario "a user can't shows detail of a not public friend codetool" do
+  #   user2 = User.create(email: "example_user2@example.com", username: "example_user2", password: "password")
+  #   drawer2 = Drawer.create(title: "The second drawer", description: "Description of second drawer", user: user2)
+  #   codetool2 = Codetool.create(title: "The second codetool", content: "Content of second codetool", drawer: drawer2, user: user2, public: false)
+
+  #   visit drawer_codetool_path(drawer2, codetool2)
+
+  #   expect(page).to have_content(codetool2.title)
+  #   expect(page).to have_content(codetool2.content)
+  #   expect(current_path).to eq(drawer_codetool_path(drawer2, codetool2))
+  # end
+
 end
