21c42cefb1b5cedea7db1fe8226702aea3f2f0ed
jrsahuquillo <jrsahuquillo@gmail.com>
2020-04-06 13:37:01 +0200

    Allows a collaborator user to show a not public codetool detail 👀

--- spec/features/codetools/show_codetool_spec.rb
+++ spec/features/codetools/show_codetool_spec.rb
@@ -2,10 +2,10 @@ require "rails_helper"
 
 RSpec.feature "Showing a Codetool" do
   before do
-    user = User.create(email: "example_user@example.com", username: "example_user", password: "password")
-    login_as(user)
-    @drawer = Drawer.create(title: "The first drawer", description: "Description of first drawer", user: user)
-    @codetool = Codetool.create(title: "The first codetool", content: "Content of codetool", drawer: @drawer, user: user, public: true)
+    @user = User.create(email: "example_user@example.com", username: "example_user", password: "password")
+    login_as(@user)
+    @drawer = Drawer.create(title: "The first drawer", description: "Description of first drawer", user: @user)
+    @codetool = Codetool.create(title: "The first codetool", content: "Content of codetool", drawer: @drawer, user: @user, public: true)
   end
 
   scenario "a user shows a codetool that belongs a drawer" do
@@ -36,8 +36,22 @@ RSpec.feature "Showing a Codetool" do
 
     expect(page).not_to have_content(codetool2.title)
     expect(page).not_to have_content(codetool2.content)
-    expect(page).to have_content("This Codetool is not public. Ask #{user2.username} to make it public to show it.")
+    expect(page).to have_content("This Codetool is not public. Ask #{user2.username} to make it public to show it")
     expect(current_path).to eq(drawer_codetool_path(drawer2, codetool2))
+  end
+
+  scenario "a user can show detail of a not public codetool of a drawer collaborated by him" do
+    user3 = User.create(email: "example_user3@example.com", username: "example_user3", password: "password")
+    drawer3 = Drawer.create(title: "The third drawer", description: "Description of third drawer", user: user3)
+    codetool3 = Codetool.create(title: "The third codetool", content: "Content of third codetool", drawer: drawer3, user: user3, public: false)
+    Friendship.create(user_id: user3.id, friend_id: @user.id)
+    DrawerCollaborator.create(drawer_id: drawer3.id, friend_id: @user.id)
+
+    visit "/drawers/#{drawer3.to_param}/codetools/#{codetool3.to_param}"
+
+    expect(page).to have_content(codetool3.title)
+    expect(page).to have_content(codetool3.content)
+    expect(current_path).to eq(drawer_codetool_path(drawer3, codetool3))
   end
 
 end
