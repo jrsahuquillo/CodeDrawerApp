32c82d8b15fcb0926099b0423fdacce57e3950c5
jrsahuquillo <jrsahuquillo@gmail.com>
2020-04-21 00:30:29 +0200

    Creates a Work Board with pinned codetools 📌

--- spec/features/codetools/pin_codetool_spec.rb
+++ spec/features/codetools/pin_codetool_spec.rb
@@ -4,32 +4,25 @@ RSpec.feature "Pin a codetool" do
   before do
     @user1 = User.create(email: "example_user1@example.com", username: "example_user1", password: "password")
     @drawer = Drawer.create(title: "The first drawer", description: "Description of first drawer", user: @user1)
-    @codetool = Codetool.create(title: "The first codetool", content: "Content of first codetool", drawer: @drawer, user: @user1, public: true)
-    # @user2 = User.create!(email: "example_user2@example.com", username: "example_user2", password: "password2")
-    # Friendship.create(user_id: @user2.id, friend_id: @user1.id)
+    @codetool1 = Codetool.create(title: "The first codetool", content: "Content of first codetool", drawer: @drawer, user: @user1, public: true)
+    PinCodetool.create(user_id: @user1.id, codetool_id: @codetool1.id)
+    @codetool2 = Codetool.create(title: "The second codetool", content: "Content of second codetool", drawer: @drawer, user: @user1, public: true)
+    PinCodetool.create(user_id: @user1.id, codetool_id: @codetool2.id)
     login_as(@user1)
+
+    visit '/pin_codetools'
   end
 
   scenario "a user pins a codetool and it is displayed in the Word Board" do
-    PinCodetool.create(user_id: @user1.id, codetool_id: @codetool.id)
-    visit '/pin_codetools'
-    expect(page).to have_content(@codetool.title)
+    expect(page).to have_content(@codetool1.title)
+    expect(page).to have_content(@codetool2.title)
   end
 
-  # scenario "a user unfavorites a friend public codetool" do
-  #   FavoriteCodetool.create(user_id: @user2.id, codetool_id: @codetool.id)
-  #   # visit "/search-friends"
-  #   # click_link @user1.username
-  #   # click_link @codetool.title
-  #   # find('.destroy-favorite').click
-  #   FavoriteCodetool.where(user_id: @user2.id, codetool_id: @codetool.id).destroy_all
-  #   expect(FavoriteCodetool.all.count).to eq(0)
-  # end
+  scenario "pinned codetool are cleared" do
+    find('.clear-pinned-codetools').click
 
-  # scenario "a user displays list of favorited codetools" do
-  #   FavoriteCodetool.create(user_id: @user2.id, codetool_id: @codetool.id)
-  #   visit "/favorite_codetools"
-  #   expect(page).to have_content(@codetool.title)
-  # end
+    expect(page).not_to have_content(@codetool1.title)
+    expect(page).not_to have_content(@codetool2.title)
+  end
 
 end
