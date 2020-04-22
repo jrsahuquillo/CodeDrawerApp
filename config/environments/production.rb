bdfd5745feee476af06ab6feb78116306b9fc303
jrsahuquillo <jrsahuquillo@gmail.com>
2020-04-22 20:28:18 +0200

    Corrects production settings to display images

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -26,10 +26,10 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = true
+  config.assets.compile = false
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
-  # config.action_controller.asset_host = 'http://assets.example.com'
+  config.action_controller.asset_host = 'http://161.35.87.109/'
 
   # Specifies the header that your server uses for sending files.
   # config.action_dispatch.x_sendfile_header = 'X-Sendfile' # for Apache
@@ -67,7 +67,7 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = false
 
   config.action_mailer.perform_caching = false
-  config.action_mailer.default_url_options = { host: 'localhost:3000' }
+  config.action_mailer.default_url_options = { host: '161.35.87.109' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
                                         address: 'smtp.gmail.com',
