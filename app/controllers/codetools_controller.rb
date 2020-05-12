98365dfcddbca0b9589e163b7ae801b0fa792b15
jrsahuquillo <jrsahuquillo@gmail.com>
2020-05-12 22:23:19 +0200

    Allows to search codetools only in a drawer scope

--- app/controllers/codetools_controller.rb
+++ app/controllers/codetools_controller.rb
@@ -22,12 +22,15 @@ class CodetoolsController < ApplicationController
   end
 
   def search
-    @searched_user_codetools = params[:search].blank? ? [] : current_user.codetools.search(params[:search])
-    @searched_friends_public_codetools = []
-    current_user.friends.each do |friend|
-      @searched_friends_public_codetools << friend.codetools.is_public.search(params[:search])
+    search_scope = params[:drawer] ? Drawer.find(params[:drawer]) : current_user
+    @searched_user_codetools = params[:search].blank? ? [] : search_scope.codetools.search(params[:search])
+    unless params[:drawer]
+      @searched_friends_public_codetools = []
+      current_user.friends.each do |friend|
+        @searched_friends_public_codetools << friend.codetools.is_public.search(params[:search])
+      end
+      @searched_friends_public_codetools = @searched_friends_public_codetools.flatten
     end
-    @searched_friends_public_codetools = @searched_friends_public_codetools.flatten
   end
 
   def new
